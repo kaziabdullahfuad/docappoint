134ad1819ba2d26adaac280b25cff15bd3843587
Kazi Abdullah Fuad <kaziabdullahfuad@gmail.com>
2026-05-20 13:20:42 +0600

    Login page ar kaaj shesh

---
+++ src/lib/auth.js
@@ -0,0 +1,20 @@
+import dns from "node:dns/promises";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("docappointdb");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    
+    client
+  }),
+   emailAndPassword: { 
+    enabled: true, 
+  }, 
+
+});
